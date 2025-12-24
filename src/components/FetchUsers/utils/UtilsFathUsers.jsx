2522230f99b1e1c2a5b8f293ac79375ffe2a63e0
Anatoliy Nakonechnyy <anatoliy.nakonechnyy@gmail.com>
2025-12-25 01:12:00 +0200

    commit 2

--- src/components/FetchUsers/utils/UtilsFathUsers.jsx
+++ src/components/FetchUsers/utils/UtilsFathUsers.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://jsonplaceholder.typicode.com/users32';
+const BASE_URL = 'https://jsonplaceholder.typicode.com/users';
 export const utilsFetchUsers = async () => {
   try {
     return await axios.get(BASE_URL);
